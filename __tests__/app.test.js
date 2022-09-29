4b0e68fdba7f157a0e9ddd9f6d4326f6c0e8987f
Bill <william.baxter@ecs.co.uk>
2022-09-29 08:48:31 +0100

    add unit testing

---
+++ __tests__/app.test.js
@@ -0,0 +1,18 @@
+const {createTextArray} = require("../helper_functions/createTextArray")
+
+describe("array function", () => {
+  test("return correct array", () => {
+    const expectedArray = [{
+      id: 0,
+      articleName: "testfile1.txt",
+      contents: "test",
+      articleSummary: "test...",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThyEKIq_a7eWEwVEoo1aTBQ6gV1KQ4BI8ojEQgnl0ITQ&s",
+    }];
+
+    const input = ["testfile1.txt"]
+    expect(createTextArray(input)).toEqual(expectedArray)
+  });
+});
+
